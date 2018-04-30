593ad4bc7657f6663d6b80a4918b7f1818fab232
ThisDudeAbides <38306431+ThisDudeAbides@users.noreply.github.com>
2018-04-29 21:48:46 -0400

    Add files via upload

--- capstone1/Program.cs
+++ capstone1/Program.cs
@@ -9,7 +9,46 @@ namespace capstone1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            while (true)
+            {
+                Console.WriteLine("Please enter a word");
+                string input = Console.ReadLine();
+
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+                int index = input.IndexOfAny(vowels);
+
+                if (index != -1)
+                {
+                    if (index == 0)
+                    {
+                        Console.WriteLine($"{input}way");
+                    }
+
+                    else 
+                    {
+                        string end = input.Substring(index);
+                        string begining = input.Substring(0, index);
+                        Console.WriteLine($"{end}{begining}ay");
+                    }
+
+                
+                            
+                }
+
+                
+            }
+        
         }
     }
 }
+
+
+
+
+
+
+        
+    
+
