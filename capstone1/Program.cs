3c9aabc81a7ba17803ed858ddb2980def44ca21b
ThisDudeAbides <38306431+ThisDudeAbides@users.noreply.github.com>
2018-05-01 20:58:01 -0400

    Add files via upload
    completed while loop and asked if user wanted to continue translating

--- capstone1/Program.cs
+++ capstone1/Program.cs
@@ -9,7 +9,7 @@ namespace capstone1
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             while (true)
             {
                 Console.WriteLine("Please enter a word");
@@ -18,29 +18,33 @@ namespace capstone1
                 char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
                 int index = input.IndexOfAny(vowels);
-
+                // singling out situation where their are no vowels in word
                 if (index != -1)
-                {
+                {   // where word starts with vowel identified in index 
                     if (index == 0)
                     {
                         Console.WriteLine($"{input}way");
                     }
 
-                    else 
-                    {
+                    else
+                    {   // if word does not start with vowel then the word splits 
+                        //
                         string end = input.Substring(index);
                         string begining = input.Substring(0, index);
                         Console.WriteLine($"{end}{begining}ay");
+                                               
                     }
+                    Console.WriteLine("would you like to continue translating? y/n");
+                    string response = (Console.ReadLine());
 
-                
-                            
+                    if (response.ToLower() != "n")
+                    {
+                        break;
+                    }
                 }
-
-                
+                Console.WriteLine("Thanks for playing, have a good day!");
             }
-        
-        }
+        }  
     }
 }
 
